67e03ebe0a90ae36cba31a786ac9dc7a648c41a3
Cristobal Dominguez <cristobald@gmail.com>
2022-06-24 21:32:36 -0400

    Agrega Rutas Show, New y Create

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,4 +2,32 @@ class BooksController < ApplicationController
     def index
         @books = Book.all
     end
+
+    def show
+        @book = Book.find(params[:id])
+    end
+
+    def new
+        @book = Book.new
+        @statuses = Book.statuses.keys
+    end
+
+    def create
+        @book = Book.new(book_params)
+
+        respond_to do |format|
+            if @book.save
+                format.html { redirect_to book_path(@book.id), notice: 'El libro fue creado exitosamente' }
+            else
+                format.html { render :new }
+            end
+        end
+    end
+
+    
+
+    private
+    def book_params
+        params.require(:book).permit(:title, :status, :author, :borrowed_at, :returned_at)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   get 'books/index'
+  get 'books/new'
+  post 'books', to: 'books#create'
+
+  # locahost:3000/books/1
+  get 'books/:id', to: 'books#show', as: 'book'
 
   root 'books#index'
 end
